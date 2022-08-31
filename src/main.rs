6629a5c29f4c34d3b051a134328273430f1dbedd
Dylan Landry <dylanglandry@gmail.com>
2022-08-31 19:19:24 -0400

    add example entities, systems, components, and resources

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use bevy::prelude::*;
+use chrono::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(MiniShrewd)
+        .run();
+}
+
+pub struct MiniShrewd;
+
+impl Plugin for MiniShrewd {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(LogTimeTimer {
+            timer: Timer::from_seconds(1.0, true),
+        })
+        .add_startup_system(add_trees)
+        .add_system(log_time)
+        .add_system(log_positions);
+    }
+}
+
+fn add_trees(mut commands: Commands) {
+    commands.spawn().insert(Position { x: 0.0, y: 0.0 });
+    commands.spawn().insert(Position { x: 1.0, y: 0.0 });
+}
+
+fn log_positions(query: Query<&Position>) {
+    for position in query.iter() {
+        println!("entity at position ({}, {})", position.x, position.y);
+    }
+}
+
+fn log_time(time: Res<Time>, mut timer: ResMut<LogTimeTimer>) {
+    if timer.timer.tick(time.delta()).just_finished() {
+        match Duration::from_std(time.time_since_startup()) {
+            Ok(run_duration) => println!(
+                "time is {}:{}:{}",
+                run_duration.num_hours(),
+                run_duration.num_minutes() % 60,
+                run_duration.num_seconds() % 60
+            ),
+            Err(_) => (),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+struct LogTimeTimer {
+    timer: Timer,
 }
